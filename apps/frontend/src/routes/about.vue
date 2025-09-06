9c91ca545a6ef15a637d5c0da50d3b058ad704b8
Dmitriy Karpov <dmtkpv@gmail.com>
2025-09-06 14:17:11 +0300

    frontend: about heading

--- apps/frontend/src/routes/about.vue
+++ apps/frontend/src/routes/about.vue
@@ -17,33 +17,13 @@
 <template>
     <main>
 
+        <h1 class="sr-only">{{ PAGES.About.title }}</h1>
+
         <l-hero :value="DATA.slides" />
 
-<!--        <ui-slider class="h-150" :value="dbSlides" />-->
-
-<!--        <l-section title="Why us" wrapper :grid="3" :gap="120">-->
-<!--            <item-why v-for="{ title, note } in dbWhy" :title="title" :note="note" />-->
-<!--        </l-section>-->
-
-<!--        <l-section title="How we work" wrapper :grid="4" :gap="80" class="bg-gary-light">-->
-<!--            <item-how v-for="item in dbHow" />-->
-<!--        </l-section>-->
-
-<!--        <section class="bg-black text-white py-20">-->
-<!--            <div class="wrapper flex gap-10 items-center justify-center">-->
-<!--                <p class="bold text-3xl">Let’s make videos that click & <span class="text-amber">Stories</span> that stick</p>-->
-
-<!--                <ui-button text="Get a free quote" />-->
-<!--            </div>-->
-<!--        </section>-->
-
-<!--        <l-section title="What we can do" wrapper :grid="2" :gap="40">-->
-<!--            <item-what v-for="({ title, note }, i) in dbWhat" :title="title" :note="note" :class="{ 'bg-gray-light': (Math.floor(i / 2) + i) % 2 }" />-->
-<!--        </l-section>-->
-
-<!--        <l-section :grid="2" title="Blog/News" class="!pb-0">-->
-<!--            <item-blog v-for="item in dbBlog" :value="item" :image="item.image" />-->
-<!--        </l-section>-->
+        <l-section title="Why Us">
+            <section-why value="" />
+        </l-section>
 
         <h1 v-for="n in 1000">A</h1>
 
@@ -58,10 +38,7 @@
 
 <script setup>
 
+    import PAGES from 'db:pages'
     import DATA from 'db:about'
-
-
-    // import { dbBlog, dbHow, dbSlides, dbWhat, dbWhy } from '~/config/database'
-    // import { UiSlider, LSection, ItemWhy, ItemHow, ItemWhat, ItemBlog, UiButton, LHero } from '~/config/components'
 
 </script>
