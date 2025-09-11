5351e1d479cea9026722427c3e967bbbbb95df85
Dmitriy Karpov <dmtkpv@gmail.com>
2025-09-11 17:41:21 +0300

    frontend: delete

--- packages/_frontend/src/config/validations.js
+++
@@ -1,7 +0,0 @@
-export function required (value) {
-    if (!value) return 'This field is required';
-}
-
-export function email (value) {
-    if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) return 'Invalid email address';
-}
