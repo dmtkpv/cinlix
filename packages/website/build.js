2a05ed9675ae0997148c784f8605d384013c3ac9
Dmitriy Karpov <dmtkpv@gmail.com>
2025-09-12 10:45:39 +0300

    website: update build script

--- packages/website/build.js
+++ packages/website/build.js
@@ -1,20 +1,19 @@
 import { fileURLToPath } from 'url'
 import { loadNuxt, buildNuxt } from '@nuxt/kit'
+import { knex } from '@this/database'
 
-export default async function runGenerate() {
+const root = fileURLToPath(new URL('.', import.meta.url));
 
-    const root = fileURLToPath(new URL('.', import.meta.url))
-
-    const nuxt = await loadNuxt({
-        cwd: root,
-        dev: false,
-        overrides: {
-            nitro: {
-                static: true
-            }
+const nuxt = await loadNuxt({
+    cwd: root,
+    dev: false,
+    overrides: {
+        nitro: {
+            static: true,
         }
-    })
+    }
+})
 
-    await buildNuxt(nuxt);
+await buildNuxt(nuxt);
+await knex.destroy();
 
-}
