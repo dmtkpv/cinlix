f58348538ba2b78291142ebf5877287d152f91e6
Dmitriy Karpov <dmtkpv@gmail.com>
2025-09-23 13:23:50 +0300

    website: fix build script

--- packages/website/build.js
+++ packages/website/build.js
@@ -20,13 +20,17 @@ await buildNuxt(nuxt);
 await knex.destroy();
 
 const distDir = join(nuxt._nitro.options.output.dir, 'public');
-const publicDir = join(root, 'caddy', 'public');
-const backupDir = join(root, 'caddy', 'backup');
+const caddyDir = join(root, 'caddy');
+const tempDir = join(caddyDir, 'temp');
+const publicDir = join(caddyDir, 'public');
+const backupDir = join(caddyDir, 'backup');
 
+fs.cpSync(distDir, tempDir, { recursive: true });
 const hasPublic = fs.existsSync(publicDir);
 if (hasPublic) fs.renameSync(publicDir, backupDir);
-fs.renameSync(distDir, publicDir);
+fs.renameSync(tempDir, publicDir);
 if (hasPublic) fs.rmSync(backupDir, { recursive: true, force: true });
+fs.rmSync(tempDir, { recursive: true, force: true });
 
 
 
